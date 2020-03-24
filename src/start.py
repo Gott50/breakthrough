19634f7d6caa186fe9913d77cdd93d00ad266228
Gott50 <xxGott50xx@gmail.com>
2020-03-24 10:13:21 +0100

    use higher factor

--- src/start.py
+++ src/start.py
@@ -1,21 +1,21 @@
 from bot.tasks import create_task
 from bot.time_util import sleep, get_time
 
-factor = 2.5
+factor = 5
+counter = 0
+
 
 def is_time():
     hour = int(get_time('this_hour'))
-    return 10 < hour < 22
+    return 9 < hour < 23
 
 
-i = 0
 while True:
     while not is_time():
         sleep(50 * 60 / factor)
 
     result = create_task(sleep=50)
-    i += result
-    print("result %s: %s " % (i, result))
+    counter += result
+    print("result %s: %s " % (counter, result))
 
     sleep(50 * 60 / factor)
-
